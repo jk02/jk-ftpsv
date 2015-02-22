26807310c8f174913e6e3dd640eafeb06c5b4619
jk <jk02@users.noreply.github.com>
2015-02-22 12:30:35 -0500

    Added FTPSession

---
+++ FTPSession.c
@@ -0,0 +1,10 @@
+// jk-ftpsv
+// Basic FTP Server
+// FTPSession.c - Governs the client-server FTP session
+// and transfers files.
+//
+// Author: jk02
+//
+// This public domain software comes with NO WARRANTY.  See LICENSE in the repository for further details.
+
+#include "jkftpsv.h"
