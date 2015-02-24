42dd7a8741e6dd4615aa3f401696fbcff953bd0f
jk <jk02@users.noreply.github.com>
2015-02-24 08:32:54 -0500

    Removed polling; got socket connection monitoring to work with recv() return val.

--- FTPSession.c
+++ FTPSession.c
@@ -15,40 +15,26 @@ int invoke_session(int cli_sock)
 {
   int ft_socket; // Socket used for file transfer
   int command_result; // Used to monitor session status (breaks if 1)
+  int recv_len; // Number of bits we receive from the client
   char command[1024]; // Buffer to hold the command sent by client
-  struct pollfd clifd[1];
   
   // Send the 220 greeting message to the client
   send_message(cli_sock,"220-Welcome to the server\n220-Proudly running jk-ftpsv\n220-\n220 Features: a .\n");
   // Note: In multithreaded implementation, may be necessary to implement 421 (temporary rejection) greeting
 
-  // Setup clifd so it is possible to poll the connection
-  memset(&clifd, 0, sizeof(clifd));
-  clifd[1].fd = cli_sock;
-  clifd.events = POLLIN | POLLOUT;
-  
   // Listen for commands using the TELNET socket
   command_result = 0; // command_result will be 0 unless QUIT is detected
-  do {
-      printf("in the loop...");
-      // Poll the socket to make sure the client has not disconnected
-      int alive;
-      printf("polling connection...");
-      alive = poll(&clifd, 1, 3500); // Poll the connection, waiting 3500ms
-      if(alive == -1)
-	perror("Error polling connection.");
-      else if(alive == 0)
-	{
-	  printf("Client disconnected or timed out.");
-	  close(cli_sock);
-	  return 0;
-	}
-      // Receive a command from the client
-      recv(cli_sock, &command, 1024, 0);
-
-      // Parse and act on the command with dispatch_command
-      command_result = dispatch_command(command);        
-  } while(command_result == 0);
+  recv_len = 1; // initialize recv_len
+  while(command_result == 0 && recv_len > 0)
+  {
+    recv_len = printf("in the loop...");
+    
+    // Receive a command from the client
+    recv_len = recv(cli_sock, &command, 1024, 0);
+    
+    // Parse and act on the command with dispatch_command
+    command_result = dispatch_command(command);        
+  }
 
   // Close the control connection
   send_message(cli_sock, "221 Bye.\n");
