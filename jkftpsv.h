92e00415e21da7babe43abc13d6825347867ab7b
jk <jk02@users.noreply.github.com>
2015-02-24 22:53:10 -0500

    Added client_session struct definition. Added function prototypes.

--- jkftpsv.h
+++ jkftpsv.h
@@ -19,10 +19,19 @@
 #ifndef FTPSV_H
 #define FTPSV_H
 
+typedef struct client_session { // struct storing key information on this session
+  int control_sock; // the TELNET socket (= cli_sock)
+  int ft_sock; // the file transfer socket (not yet opened)
+  char* uid; // username set by the USER command
+} client_session;
+
 int make_socket(uint16_t);
 int invoke_session(int);
-int dispatch_command(char*);
+int dispatch_command(char*,client_session*);
 void send_message(int,char*);
+
+// Begin command handling function prototypes
+void cmd_noop(client_session*);
 
 #endif /* FTPSV_H */
 
